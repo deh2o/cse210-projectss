f8fcfa0b3ad44f5d25e821487da388f11aa7e5a4
A I Walter <67634301+deh2o@users.noreply.github.com>
2026-05-10 16:17:31 +0100

    Complete week1 exercise 4

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -1,28 +1,31 @@
 using System;
-using System.Collections.Generic; //importing the Collections.Generic namespace to use List<T>
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        List<int> numbers = new List<int>(); // Creating a new list to store numbers
+        List<int> numbers = new List<int>();
+        
+        Console.WriteLine("Welcome to the Program!\n"); 
 
-        //Lets welcome users to the program
-        Console.WriteLine("Welcome to the Program!");
-
-        //for readability, lets leave a line.
-        Console.WriteLine("");
-
-        // Get user inputs to append to the list
-        Console.WriteLine("Enter a number to add to the list (or type '0' to finish):");
-        int userInput = int.Parse(Console.ReadLine());
-
-        // set a loop to continue accepting user input until they type '0'
-        while (userInput != 0)
+        // The loop needs to ask for a NEW input every time it runs
+         int inputNumber = -1;
+        while ( inputNumber != 0)
         {
-            numbers.Add(userInput);
-        }
-
+            Console.WriteLine("Enter a number (or enter 0 to quit");
+            string userInput = Console.ReadLine();
+            
+            //parse the input into a digit
+            inputNumber = int.Parse(userInput);
+            
+            //lets add numbers when its not 0
+            if ( inputNumber != 0)
+            {
+            	numbers.Add(inputNumber);
+            }
+        } 
+        
         // Set an end to user input and print the list of numbers
         Console.WriteLine(" Input Complete! Below is your input Result");
 
@@ -30,7 +33,30 @@ class Program
         Console.WriteLine("");
 
         // Print the list of numbers entered by the user
-        Console.WriteLine($"Your List of Numbers:" + "" + numbers + "," + "");
-
+        Console.WriteLine($"Your List of Numbers: {string.Join(", ", numbers)}");
+        
+        //Calculator the sum
+        int sum = 0;
+        foreach (int number in numbers)
+        {
+            sum += number;
+        }
+        Console.WriteLine($"the sum of the numbers is: {sum}");
+        
+        //calculate the average 
+        float average = sum / numbers.Count;
+        Console.WriteLine($"the average is: {average}");
+        
+        //lets try and check max number
+        int max = numbers[0];
+        
+        foreach (int number in numbers)
+        {
+            if (number > max)
+            {
+                max = number;
+            }
+        }
+        Console.WriteLine($"the maximum number is: {max}");
     }
 }
