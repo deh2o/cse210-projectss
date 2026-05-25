96e34111c886f09b4ccafdc43bbc6900ff7ed63b
deh2o <iagha1@byupathway.edu>
2026-05-25 17:08:28 +0100

    W03 SCripture Memorizer Program

--- week03/ScriptureMemorizer/Program.cs
+++ week03/ScriptureMemorizer/Program.cs
@@ -1,9 +1,63 @@
+// EXCEEDING REQUIREMENTS:
+// This program stores multiple scriptures and randomly selects one
+// each time the program runs.
+
 using System;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the ScriptureMemorizer Project.");
+
+        // Create a list of scriptures
+        List<Scripture> scriptures = new List<Scripture>();
+
+        // Add some scriptures to the list
+        scriptures.Add(new Scripture(
+            new Reference("John", 3, 16),
+            "For God so loved the world that he gave his one and only Son that whoever believes in him shall not perish but have eternal life."
+        ));
+
+        scriptures.Add(new Scripture(
+            new Reference("Proverbs", 3, 5, 6),
+            "Trust in the Lord with all your heart and lean not on your own understanding in all your ways submit to him and he will make your paths straight."
+        ));
+
+        scriptures.Add(new Scripture(
+            new Reference("Psalm", 23, 1),
+            "The Lord is my shepherd I shall not want."
+        ));
+
+        // Randomly select a scripture from the list
+        Random random = new Random();
+        Scripture selectedScripture = scriptures[random.Next(scriptures.Count)];
+
+        // Loop to hide words until the scripture is completely hidden
+        while (!selectedScripture.IsCompletelyHidden())
+        {
+            Console.Clear();
+
+            Console.WriteLine(selectedScripture.GetDisplayText());
+
+            Console.WriteLine();
+            Console.WriteLine("Press ENTER to continue or type 'quit' to finish:");
+
+            string input = Console.ReadLine();
+
+            if (input.ToLower() == "quit")
+            {
+                break;
+            }
+
+            selectedScripture.HideRandomWords(3);
+        }
+
+
+        // Final display of the scripture after the loop ends
+        Console.Clear();
+        Console.WriteLine(selectedScripture.GetDisplayText());
+
+        Console.WriteLine();
+        Console.WriteLine("Program ended.");
     }
 }
---
+++ week03/ScriptureMemorizer/Scripture.cs
@@ -0,0 +1,72 @@
+using System;
+
+class Scripture
+{
+    // This class will hold the reference and the text of a scripture, as well as a list of Word objects that represent each word in the scripture.
+    private Reference _reference;
+    private List<Word> _words;
+
+    // Constructor that takes a Reference object and a string of text, splits the text into words, and creates Word objects for each word in the scripture.
+    public Scripture(Reference reference, string text)
+    {
+        _reference = reference;
+        _words = new List<Word>();
+
+        string[] splitWords = text.Split(' ');
+
+        foreach (string word in splitWords)
+        {
+            _words.Add(new Word(word));
+        }
+    }
+
+    // Constructor to randomly hide a specified number of words in the scripture. It will continue to hide words until the specified number is hidden or the scripture is completely hidden.
+    public void HideRandomWords(int numberToHide)
+    {
+        Random random = new Random();
+
+        int hiddenCount = 0;
+
+        while (hiddenCount < numberToHide)
+        {
+            int index = random.Next(_words.Count);
+
+            if (!_words[index].IsHidden())
+            {
+                _words[index].Hide();
+                hiddenCount++;
+            }
+
+            if (IsCompletelyHidden())
+            {
+                break;
+            }
+        }
+    }
+
+    // Getter to cified number of words in the scripture. It will continue to hide words until the specified number is hidden or the scripture is completely hidden.
+    public string GetDisplayText()
+    {
+        string displayText = _reference.GetDisplayText() + " ";
+
+        foreach (Word word in _words)
+        {
+            displayText += word.GetDisplayText() + " ";
+        }
+
+        return displayText;
+    }
+
+    public bool IsCompletelyHidden()
+    {
+        foreach (Word word in _words)
+        {
+            if (!word.IsHidden())
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
